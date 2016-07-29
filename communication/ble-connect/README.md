a8ebaca118e297f8614f1ad5c09a79998e7c28b1
sojamo <andi@sojamo.de>
2016-07-29 11:52:44 +0800

    adding more details to console.log out

--- communication/ble-connect/README.md
+++ communication/ble-connect/README.md
@@ -34,3 +34,32 @@ It should display the name of the available devices (something like TwizXXX).
 You need to enable it in the beginning of index.js and it should work.
 
 If you have a new Twiz firmware, move it to get data, otherwise it will seem dead.
+
+
+#### Advertising
+
+I ran into the following issues while attempting to  connect multiple  Twiz to the ble-connect app:
+
+  - only one twiz would be recognized at certain times after starting the app
+	- at certain times a second twiz would not be recognized
+
+After scratching my head around this for a while, I conclude with the following findings:
+
+  - all Twiz must be available when starting the app in order to be recognized and able to connect. They simply have to on when the app starts.
+	- when a Twiz is disconnected or looses power after having been connected before, it will be able to reconnect (Having said that, sometimes this is not true, the Twiz will not reconnect, need to restart the app)
+  - Having said all this, there are situations where things work as expected, not matter of the state of a Twiz
+	- Reintroducing the restart of the scanning process after given time intervals also seemed to improve this hiccup `setTimeout(function() { scan(); }, 10000);`
+
+#### LED
+
+There are 3 states that a Twiz is able to communicate via the on board LED
+
+  - constant green: booting
+	- blinking green: ready to connect
+	- blinking blue : connected and sending data 
+	
+
+
+
+
+
--- communication/ble-connect/index.js
+++ communication/ble-connect/index.js
@@ -17,8 +17,6 @@ var data = {};
 var address = "/twiz"; /* default osc address pattern */
 // address = "/wek/inputs"; /* wekinator address pattern */
 
-var remote = "127.0.0.1";
-
 var port = 11000; /* default osc port */
 // port = 5000; /* GRT */
 // port = 6448; /* wekinator */
@@ -37,41 +35,41 @@ debugMessages.push('queue');
 debugMessages.push('osc');
 
 
-debug('info','sending data to remote address '+config.remote);
+debug('info','sending data to remote address '+config.remote+":"+port);
 
 
 
 var sock = udp.createSocket("udp4", function(thePacket, rinfo) {
-  // receive an OSC message
-  debug('osc','received an OSC packet at '+wsListeningPort);
-  try {
-    // send through WebSocketServer wss to all connected clients (browser)
-    var packet = unpack(thePacket);
-    debug('osc',packet);
+	// receive an OSC message
+	debug('osc','received an OSC packet at '+wsListeningPort);
+	try {
+		// send through WebSocketServer wss to all connected clients (browser)
+		var packet = unpack(thePacket);
+		debug('osc',packet);
 
-    /* a packet here is converted into an object which is
-     * identifiable by its oscType value (message or bundle).
-     * at this point the packet, no matter of which type it is,
-     * can be sent to the client, or can be processed and
-     * scheduled (if of type bundle) for distribution considering
-     * the timetag.
-     * the packet is the sent to the Websocket client where it is
-     * translated from the osc-min packet format into an
-     * sojamo.osc.OscPacket format.
-     */
+		/* a packet here is converted into an object which is
+		 * identifiable by its oscType value (message or bundle).
+		 * at this point the packet, no matter of which type it is,
+		 * can be sent to the client, or can be processed and
+		 * scheduled (if of type bundle) for distribution considering
+		 * the timetag.
+		 * the packet is the sent to the Websocket client where it is
+		 * translated from the osc-min packet format into an
+		 * sojamo.osc.OscPacket format.
+		 */
 
-    // wss.clients.forEach(function each(client) {
-    //   client.send(packet);
-    // });
+		// wss.clients.forEach(function each(client) {
+		//   client.send(packet);
+		// });
 
-  } catch (err) {
-    logger.log('error',err);
-    return logger.log('debug','invalid OSC packet.');
-  }
+	} catch (err) {
+		logger.log('error',err);
+		return logger.log('debug','invalid OSC packet.');
+	}
 });
 
 var unpack = function(thePacket) {
-    return JSON.stringify(osc.fromBuffer(thePacket));
+	return JSON.stringify(osc.fromBuffer(thePacket));
 }
 
 // var oscListeningPort = 11000;
@@ -82,192 +80,215 @@ var unpack = function(thePacket) {
 var noble = require('noble');
 
 function scan() {
-  debug('scan', 'restart scanning ble devices ..');
+	debug('scan', 'restart scanning ble devices ..');
 
-  setTimeout(function() { noble.startScanning(); }, 500);
-  // setTimeout(function() { scan(); }, 10000);
+	setTimeout(function() { noble.startScanning(); }, 500);
 
-  /*
-   * use intervals to restart scanning, see:
-   * https://github.com/sandeepmistry/noble/issues/97
-   *
-   * In case a peripheral disconnects due to crashing, running out of power, etc.
-   * it can only reconnect after refreshing the scanning procedure by calling
-   * noble.startScanning() this has been a solution reported under issue 97
-   * though this occasionally resulted in conflicts after a while, again see issue 97.
-   *
-   * Also consider to change the scanning procedure to the method used in 'reconnect.js'
-   *
-   */
+	setTimeout(function() { scan(); }, 10000);
+
+	/*
+	 * use intervals to restart scanning, see:
+	 * https://github.com/sandeepmistry/noble/issues/97
+	 *
+	 * In case a peripheral disconnects due to crashing, running out of power, etc.
+	 * it can only reconnect after refreshing the scanning procedure by calling
+	 * noble.startScanning() this has been a solution reported under issue 97
+	 * though this occasionally resulted in conflicts after a while, again see issue 97.
+	 *
+	 * Also consider to change the scanning procedure to the method used in 'reconnect.js'
+	 *
+	 */
 }
 
 noble.on('stateChange', function(state) {
-  if (state === 'poweredOn') {
-    scan();
-  } else {
-    debug('scan', 'stop scanning ... ');
-    noble.stopScanning();
-  }
+	if (state === 'poweredOn') {
+		scan();
+	} else {
+		debug('scan', 'stop scanning ... ');
+		noble.stopScanning();
+	}
 });
 
 
 noble.on('disconnect', function(peripheral) {
-  debug('disconnect', 'disconnected from ' + peripheral);
+	debug('disconnect', 'disconnected from ' + peripheral);
 });
 
 
 noble.on('connect', function(peripheral) {
-  debug('connect', 'connected to ' + peripheral);
+	debug('connect', 'connected to ' + peripheral);
 });
 
 
 
 noble.on('discover', function(peripheral) {
 
-  var name = peripheral.advertisement.localName;
-  var delay = 100;
+	var name = peripheral.advertisement.localName;
+	var delay = 100;
 
 
-  debug('connect', 'discovered: '+peripheral.advertisement.localName+" ( " +(twiz.indexOf(name) > -1 ? "registered":"not accepted" )+ " )");
+	debug('connect', 'discovered: '+peripheral.advertisement.localName+" ( " +(twiz.indexOf(name) > -1 ? "registered":"not accepted" )+ " )");
 
-  if( twiz.indexOf(name) > -1 ) {
-    debug('connect', 'Starting to connect to ' + name + ', waiting ' + delay + 'ms.');
+	if( twiz.indexOf(name) > -1 ) {
+		debug('connect', 'Starting to connect to ' + name + ', waiting ' + delay + 'ms.');
 
-    setTimeout(function() {
-      debug('connect', 'attempting to connect to ' + name);
+		setTimeout(function() {
+			debug('connect', 'attempting to connect to ' + name);
 
-    /* TODO Timeout of 5 seconds required? Or bug fixed? */
+			/* TODO Timeout of 5 seconds required? Or bug fixed? */
 
-    peripheral.once('connect',function() {
-      debug('connect', 'connected to ' + peripheral.advertisement.localName);
-      data[peripheral.advertisement.localName].isConnected = true;
-    });
+			peripheral.once('connect',function() {
+				debug('connect', 'connected to ' + peripheral.advertisement.localName);
+				data[peripheral.advertisement.localName].isConnected = true;
+			});
 
-    peripheral.once('disconnect',function() {
-      debug('disconnect', 'Got disconnected from ' + peripheral.advertisement.localName);
-      data[peripheral.advertisement.localName].isConnected = false;
-      scan();
-    });
+			peripheral.once('disconnect',function() {
+				debug('disconnect', 'Got disconnected from ' + peripheral.advertisement.localName);
+				data[peripheral.advertisement.localName].isConnected = false;
+				scan();
+			});
 
-    peripheral.connect(function(error) {
-      debug('connect', 'connected to peripheral: ' + peripheral.advertisement.localName +' ( '+ peripheral.uuid+ ' )');
+			peripheral.connect(function(error) {
+				debug('connect', 'connected to peripheral: ' + peripheral.advertisement.localName +' ( '+ peripheral.uuid+ ' )');
 
-      peripheral.discoverServices(['1901'], function(error, services) {
-        debug('discover', 'discovered service:'+services[0]);
+				peripheral.discoverServices(['1901'], function(error, services) {
+					debug('discover', 'discovered service:'+services[0]);
 
-        var service = services[0];
+					var service = services[0];
 
-        service.discoverCharacteristics(['2b01'], function(error, characteristics) {
-          debug('discover', 'discovered characteristic');
+					service.discoverCharacteristics(['2b01'], function(error, characteristics) {
+						debug('discover', 'discovered characteristic');
 
-          var characteristic = characteristics[0];
-          characteristic.on('read', function(theData, isNotification) {
+						var characteristic = characteristics[0];
+						characteristic.on('read', function(theData, isNotification) {
 
-            if(theData.length==12) {
-              var values = [];
-              for(var k=0;k<theData.length;k+=2) {
-                  values.push(theData.readInt16BE(k, 2)); /* signed int */
-              }
-		debug('data',
-		      peripheral.advertisement.localName+"\t"+values[0]+
-		      ", "+values[1]+
-		      ", "+values[2]+
-		      ", "+values[3]+
-		      ", "+values[4]+
-		      ", "+values[5]);
+							if(theData.length==12) {
+								var values = [];
+								for(var k=0;k<theData.length;k+=2) {
+									values.push(theData.readInt16BE(k, 2)); /* signed int */
+								}
+								debug('data',
+										peripheral.advertisement.localName+"\t"+values[0]+
+										", "+values[1]+
+										", "+values[2]+
+										", "+values[3]+
+										", "+values[4]+
+										", "+values[5]);
 
-              var n = Math.pow(2, 16);
-              var rad = 0.0174533;
-              var ax = (values[0] * 4.0)/n;
-              var ay = (values[1] * 4.0)/n;
-              var az = (values[2] * 4.0)/n;
+								var n = Math.pow(2, 16);
+								var rad = 0.0174533;
+								var ax = (values[0] * 4.0)/n;
+								var ay = (values[1] * 4.0)/n;
+								var az = (values[2] * 4.0)/n;
 
-              var ex = rad * (values[3] * 360.0)/n;
-              var ey = rad * (values[4] * 360.0)/n;
-              var ez = rad * (values[5] * 360.0)/n;
+								var ex = rad * (values[3] * 360.0)/n;
+								var ey = rad * (values[4] * 360.0)/n;
+								var ez = rad * (values[5] * 360.0)/n;
 
-              debug('debug',az, values[2], theData );
 
-              var queue = data[peripheral.advertisement.localName].queue;
+								var queue = data[peripheral.advertisement.localName].queue;
 
-              if(queue.getLength()>10) {
-                debug('queue', 'removed ' +queue.getLength() + ' items from queue.');
-                queue.clear();
-              }
-              queue.enqueue({'ex':ex, 'ey':ey, 'ez':ez, 'ax':ax, 'ay':ay, 'az':az });
-            }
-          });
+								debug('debug',
+										peripheral.advertisement.localName,
+										"x:"+ax.toFixed(3),
+										"y:"+ay.toFixed(3),
+										"z:"+az.toFixed(3),
+										queue.getLength());
 
-          // true to enable notify
-          characteristic.notify(true, function(error) {
-            debug( 'connect', 'start listening to ' + peripheral.advertisement.localName);
-          });
 
-        });
-      });
-    });
+								if(queue.getLength()>10) {
+									debug('queue', 'removed ' +queue.getLength() + ' items from queue.');
+									queue.clear();
+								}
+								queue.enqueue({'ex':ex, 'ey':ey, 'ez':ez, 'ax':ax, 'ay':ay, 'az':az });
+							}
+						});
 
-  }, delay);
+						// true to enable notify
+						characteristic.notify(true, function(error) {
+							debug( 'connect', 'start listening to ' + peripheral.advertisement.localName);
+						});
 
-  }
+					});
+				});
+			});
+
+		}, delay);
+
+	} 
 });
 
-function debug(type, msg) {
-  if(debugMessages.indexOf(type)>-1) {
-    console.log(msg);
-  }
+var time = getTime();
+
+function getTime() {
+	return new Date().getTime();
+}
+
+function debug() {
+	var type = arguments[0];
+	if(debugMessages.indexOf(type)>-1) {
+		var t = getTime();
+		var d = t-time;
+		time = t;
+		var msg = new Date().getTime() + " ("+d+")\t";
+		for(var i=1;i<arguments.length;i++){
+			msg += arguments[i]+"\t";
+		}
+		console.log(msg)
+	}
 }
 
 
 function setup() {
-  for(var i in twiz) {
-      data[twiz[i]] = {'queue': new Queue(), 'isConnected':false, 'current':{'ax':0, 'ay':0, 'az':0, 'ex':0, 'ey':0, 'ez':0}};
-  }
-  loop();
+	for(var i in twiz) {
+		data[twiz[i]] = {'queue': new Queue(), 'isConnected':false, 'current':{'ax':0, 'ay':0, 'az':0, 'ex':0, 'ey':0, 'ez':0}};
+	}
+	loop();
 }
 
+var frameRate = 50;
 
 function loop() {
-    setTimeout(function() {
+	setTimeout(function() {
 
-      var interpolate = true;
+		var interpolate = true;
 
-      for(var key in data) {
-          // console.log('reading '+key+' '+data[key].queue.getLength());
-          if(data[key].isConnected === false) {
-            continue;
-          }
-          var state = data[key].queue.dequeue();
-          if(state !== undefined) {
-            data[key].current.ax = state.ax;
-            data[key].current.ay = state.ay;
-            data[key].current.az = state.az;
-            data[key].current.ex = state.ex;
-            data[key].current.ey = state.ey;
-            data[key].current.ez = state.ez;
-          }
+		for(var key in data) {
+			// console.log('reading '+key+' '+data[key].queue.getLength());
+			if(data[key].isConnected === false) {
+				continue;
+			}
+			var state = data[key].queue.dequeue();
+			if(state !== undefined) {
+				data[key].current.ax = state.ax;
+				data[key].current.ay = state.ay;
+				data[key].current.az = state.az;
+				data[key].current.ex = state.ex;
+				data[key].current.ey = state.ey;
+				data[key].current.ez = state.ez;
+			}
 
-          var buf;
-          buf = osc.toBuffer({
-            address: ("/twiz/"+key),
-              args: [key,
-		     data[key].current.ax,
-		     data[key].current.ay,
-		     data[key].current.az,
-		     data[key].current.ex,
-		     data[key].current.ey,
-		     data[key].current.ez ]
-          });
-          sock.send(buf, 0, buf.length, port, config.remote);
-      }
+			var buf;
+			buf = osc.toBuffer({
+				address: ("/twiz/"+key),
+				args: [key,
+				data[key].current.ax,
+				data[key].current.ay,
+				data[key].current.az,
+					data[key].current.ex,
+					data[key].current.ey,
+						data[key].current.ez ]
+			});
+			sock.send(buf, 0, buf.length, port, config.remote);
+		}
 
-      loop();
+		loop();
 
-    },20);
+	},1000/frameRate);
 }
 
 function almostEqual(a,b,epsilon) {
-  return Math.abs(a - b) < epsilon;
+	return Math.abs(a - b) < epsilon;
 }
 
 setup();
@@ -275,14 +296,14 @@ setup();
 
 /*
 
-Queue.js
+	 Queue.js
 
-A function to represent a queue
+	 A function to represent a queue
 
-Created by Stephen Morley - http://code.stephenmorley.org/ - and released under
-the terms of the CC0 1.0 Universal legal code:
+	 Created by Stephen Morley - http://code.stephenmorley.org/ - and released under
+	 the terms of the CC0 1.0 Universal legal code:
 
-http://creativecommons.org/publicdomain/zero/1.0/legalcode
+	 http://creativecommons.org/publicdomain/zero/1.0/legalcode
 
 */
 
@@ -291,60 +312,60 @@ http://creativecommons.org/publicdomain/zero/1.0/legalcode
  */
 function Queue(){
 
-  // initialise the queue and offset
-  var queue  = [];
-  var offset = 0;
+	// initialise the queue and offset
+	var queue  = [];
+	var offset = 0;
 
-  // Returns the length of the queue.
-  this.getLength = function(){
-    return (queue.length - offset);
-  }
+	// Returns the length of the queue.
+	this.getLength = function(){
+		return (queue.length - offset);
+	}
 
-  this.clear = function() {
-      queue = [];
-      offset = 0;
-  }
+	this.clear = function() {
+		queue = [];
+		offset = 0;
+	}
 
-  // Returns true if the queue is empty, and false otherwise.
-  this.isEmpty = function(){
-    return (queue.length == 0);
-  }
+	// Returns true if the queue is empty, and false otherwise.
+	this.isEmpty = function(){
+		return (queue.length == 0);
+	}
 
-  /* Enqueues the specified item. The parameter is:
-   *
-   * item - the item to enqueue
-   */
-  this.enqueue = function(item){
-    queue.push(item);
-  }
+	/* Enqueues the specified item. The parameter is:
+	 *
+	 * item - the item to enqueue
+	 */
+	this.enqueue = function(item){
+		queue.push(item);
+	}
 
-  /* Dequeues an item and returns it. If the queue is empty, the value
-   * 'undefined' is returned.
-   */
-  this.dequeue = function(){
+	/* Dequeues an item and returns it. If the queue is empty, the value
+	 * 'undefined' is returned.
+	 */
+	this.dequeue = function(){
 
-    // if the queue is empty, return immediately
-    if (queue.length == 0) return undefined;
+		// if the queue is empty, return immediately
+		if (queue.length == 0) return undefined;
 
-    // store the item at the front of the queue
-    var item = queue[offset];
+		// store the item at the front of the queue
+		var item = queue[offset];
 
-    // increment the offset and remove the free space if necessary
-    if (++ offset * 2 >= queue.length){
-      queue  = queue.slice(offset);
-      offset = 0;
-    }
+		// increment the offset and remove the free space if necessary
+		if (++ offset * 2 >= queue.length){
+			queue  = queue.slice(offset);
+			offset = 0;
+		}
 
-    // return the dequeued item
-    return item;
+		// return the dequeued item
+		return item;
 
-  }
+	}
 
-  /* Returns the item at the front of the queue (without dequeuing it). If the
-   * queue is empty then undefined is returned.
-   */
-  this.peek = function(){
-    return (queue.length > 0 ? queue[offset] : undefined);
-  }
+	/* Returns the item at the front of the queue (without dequeuing it). If the
+	 * queue is empty then undefined is returned.
+	 */
+	this.peek = function(){
+		return (queue.length > 0 ? queue[offset] : undefined);
+	}
 
 }
